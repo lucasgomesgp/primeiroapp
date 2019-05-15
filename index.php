1437e964797a62a77b59e43a5903041c55e4c0a1
Lucas Gomes <lucasjati2015@gmail.com>
2019-05-14 22:51:21 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Meu Primeiro App</title>
+</head>
+<body>
+    <?php
+        echo "Está parte está sendo criada pelo PHP!"; 
+    ?>
+</body>
+</html>
